49cffe5a24b37a029afdae69acea68f1dc234830
AnthonyNickson <Nicksonaaireent@gmail.com>
2021-07-06 16:25:27 -0600

    set up models

--- cal_app/admin.py
+++ cal_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from call_app.models import MyUser, Goal
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(MyUser, UserAdmin)
+admin.site.register(Goal)
--- cal_app/models.py
+++ cal_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class MyUser(AbstractUser):
+    name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=40)
+
+
+class Goal(models.Model):
+    dream = models.CharField(max_length=200)
+    goal = models.CharField(max_length=200)
+    what = models.TextField()
+    when = models.DateTimeField(auto_now_add=True)
+    who = models.TextField()
+    assigned_by = models.ForeignKey(
+        MyUser,
+        related_name="assigned_by",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )

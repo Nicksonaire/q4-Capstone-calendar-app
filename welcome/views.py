b52cf8e1739064ddd3fa3ba2f7f62b570bf32a71
tysherman <64978736+tysherman@users.noreply.github.com>
2021-07-20 18:33:41 -0700

    end points on buttoons work

--- welcome/views.py
+++ welcome/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from cal_app.models import MyUser
 
 # Create your views here.
 def welcome_view(request):
-
+    if request.user.is_authenticated:
+        return HttpResponseRedirect("/user/{request.user.username}")
     return render(request, "index.html")
